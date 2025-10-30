07efcc48f952a222eb283dc960f16dbcf5c7b3ce
Abhi24052004 <at0633058@gmail.com>
2025-10-30 16:39:46 +0530

    Update image source based on vehicle type

--- Frotend/src/components/ConfirmRide.jsx
+++ Frotend/src/components/ConfirmRide.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 const ConfirmRide = (props) => {
+    const [link,setLink]=React.useState("");
+    useEffect(()=>{
+        const vehicle=props.vehicleType;
+        if(vehicle==="car"){
+            setLink("https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg");
+        }
+        else if(vehicle==="moto"){
+            setLink("https://shorturl.at/5zCIa");
+        }
+        else if(vehicle==="auto"){
+            setLink("https://shorturl.at/B2YCj");
+        }
+    },[props.vehicleType])
     return (
         <div>
             <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
@@ -9,7 +23,7 @@ const ConfirmRide = (props) => {
             <h3 className='text-2xl font-semibold mb-5'>Confirm your Ride</h3>
 
             <div className='flex gap-2 justify-between flex-col items-center'>
-                <img className='h-20' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
+                <img className='h-20' src={link} alt="" />
                 <div className='w-full mt-5'>
                     <div className='flex items-center gap-5 p-3 border-b-2'>
                         <i className="ri-map-pin-user-fill"></i>
