324bc9876077ff8fec4ecad8119392575193e9ac
Abhi24052004 <at0633058@gmail.com>
2025-10-30 17:03:44 +0530

    Refactor UserProtectedWrapper to UserProtectWrapper

--- Frotend/src/pages/UserProtectedWrapper.jsx
+++ Frotend/src/pages/UserProtectedWrapper.jsx
@@ -1,19 +1,49 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react';
+import axios from 'axios'
 
-function UserProtectedWrapper({children}) {
-    const token=localStorage.getItem("token");
-    const navigate=useNavigate();
-    useEffect(() => {if(!token){
-    
-        navigate("/user/login");
-    }}, [token])
-    
-    
-  return (
-    <div>{children}</div>
-  )
+const UserProtectWrapper = ({
+    children
+}) => {
+    const token = localStorage.getItem('token')
+    const navigate = useNavigate()
+    const { user, setUser } = useContext(UserDataContext)
+    const [ isLoading, setIsLoading ] = useState(true)
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/user/login')
+        }
+
+        axios.get(`${import.meta.env.VITE_BASE_URL}/users/getprofile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                setUser(response.data)
+                setIsLoading(false)
+            }
+        })
+            .catch(err => {
+                console.log(err)
+                localStorage.removeItem('token')
+                navigate('/login')
+            })
+    }, [ token ])
+
+    if (isLoading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    )
 }
 
-export default UserProtectedWrapper
+export default UserProtectWrapper
