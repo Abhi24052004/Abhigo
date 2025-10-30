3bef24ec626a842a49e402393a24b9d4eb3f0f59
Abhi24052004 <at0633058@gmail.com>
2025-10-30 16:39:18 +0530

    Refactor CaptainDetail component to use context

--- Frotend/src/components/CaptainDetail.jsx
+++ Frotend/src/components/CaptainDetail.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
 
-function CaptainDetail() {
+import React, { useContext, useEffect } from 'react'
+import { CaptainDataContext } from '../context/CapatainContext'
+
+const CaptainDetails = () => {
+    
+    useEffect(() => {}, [])
+    const { captain } = useContext(CaptainDataContext)
+    console.log(captain.fullname.firstname);
+    
     return (
-        <div className=" lg:w-[95%] lg:ml-2">
-            <div className="flex justify-between  mx-2 mb-4 items-center bg-amber-100 rounded-sm px-2 py-2 ">
-                <div className="flex gap-2 items-center">
-                    <img className="h-15 w-15 rounded-full" src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHw" alt="" />
-                    <h4 className="text-base font-bold">Abhishek Tiwari</h4>
+        <div>
+            <div className='flex items-center justify-between'>
+                <div className='flex items-center justify-start gap-3'>
+                    <img className='h-10 w-10 rounded-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdlMd7stpWUCmjpfRjUsQ72xSWikidbgaI1w&s" alt="" />
+                    <h4 className='text-lg font-medium capitalize'>{captain.fullname.firstname + " " + captain.fullname.lastname}</h4>
                 </div>
-                <div className="">
-                    <p className="font-bold"><i className="ri-money-rupee-circle-line"></i>252.20 </p>
-                    <p className="text-sm text-gray-400 font-light text-center">Earnings</p>
+                <div>
+                    <h4 className='text-xl font-semibold'>₹300.20</h4>
+                    <p className='text-sm text-gray-600'>Earned</p>
                 </div>
             </div>
-            <div className=" flex h-40  lg:justify-between  ml-2 mb-9 gap-8 bg-yellow-200 py-7 px-3 rounded-sm">
-                <div className="flex flex-col items-center lg:ml-80"><i className="ri-time-line text-3xl"></i><p className="text-2xl font-semibold ">10.5</p><p className="text-sm text-gray-400 font-light">Hours Online</p></div>
-                <div className="flex flex-col items-center"><i className="ri-speed-up-line text-3xl"></i><p className="text-2xl font-semibold ">10.5</p><p className="text-sm text-gray-400 font-light">Distance Covered</p></div>
-                <div className="flex flex-col items-center lg:mr-80"><i className="ri-booklet-line text-3xl"></i><p className="text-2xl font-semibold ">10.5</p><p className="text-sm text-gray-400 font-light">Hours Online</p></div>
+            <div className='flex p-3 mt-8 bg-gray-100 rounded-xl justify-center gap-5 items-start'>
+                <div className='text-center'>
+                    <i className="text-3xl mb-2 font-thin ri-timer-2-line"></i>
+                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <p className='text-sm text-gray-600'>Hours Online</p>
+                </div>
+                <div className='text-center'>
+                    <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
+                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <p className='text-sm text-gray-600'>Hours Online</p>
+                </div>
+                <div className='text-center'>
+                    <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
+                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <p className='text-sm text-gray-600'>Hours Online</p>
+                </div>
+
             </div>
         </div>
     )
 }
 
-export default CaptainDetail
+export default CaptainDetails
