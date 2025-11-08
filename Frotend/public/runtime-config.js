f49c350220c9d41c9ec1274f19c9ea8895fabc5c
Abhi24052004 <at0633058@gmail.com>
2025-11-08 16:48:53 +0530

    Update runtime-config.js

--- Frotend/public/runtime-config.js
+++ Frotend/public/runtime-config.js
@@ -1,4 +1,3 @@
 window.__RUNTIME_CONFIG__ = {
-  VITE_BASE_URL: "https://abhigo-backend.onrender.com",
-  VITE_GOOGLE_MAPS_API_KEY=AIzaSyDv759NWGTvbevQqK_88SQx8UPonye-ggw
+  VITE_BASE_URL: "https://abhigo-backend.onrender.com"
 };
