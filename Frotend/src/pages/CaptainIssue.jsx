7f790f9f9e00d2ac254f10c2157b892041549a73
Abhi24052004 <at0633058@gmail.com>
2025-11-08 14:04:15 +0530

    Update CaptainIssue.jsx

--- Frotend/src/pages/CaptainIssue.jsx
+++ Frotend/src/pages/CaptainIssue.jsx
@@ -1,7 +1,14 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 const CaptainIssue = () => {
+    const navigate = useNavigate();
+    function handle(){
+        navigate('/captain/start');
+    }
     return (
         <div>
             <h1>Captain Issue Page</h1>
+            <button onClick={handle}>way</button>
         </div>
     );
 };
