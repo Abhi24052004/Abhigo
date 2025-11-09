0dafa4e57e4f69f86b639d05a1d51b57c09a1b8e
Abhi24052004 <at0633058@gmail.com>
2025-11-10 03:34:37 +0530

    Update Riding.jsx

--- Frotend/src/pages/Riding.jsx
+++ Frotend/src/pages/Riding.jsx
@@ -29,7 +29,7 @@ const Riding = () => {
                     <div className='text-right'>
                         <h2 className='text-lg font-medium capitalize'>{ride?.captain.fullname.firstname}</h2>
                         <h4 className='text-xl font-semibold -mt-1 -mb-1'>{ride?.captain.vehicle.plate}</h4>
-                        <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p>
+                        <p className='text-sm text-gray-600'></p>
 
                     </div>
                 </div>
@@ -40,7 +40,7 @@ const Riding = () => {
                         <div className='flex items-center gap-5 p-3 border-b-2'>
                             <i className="text-lg ri-map-pin-2-fill"></i>
                             <div>
-                                <h3 className='text-lg font-medium'>562/11-A</h3>
+                                <h3 className='text-lg font-medium'></h3>
                                 <p className='text-sm -mt-1 text-gray-600'>{ride?.destination}</p>
                             </div>
                         </div>
