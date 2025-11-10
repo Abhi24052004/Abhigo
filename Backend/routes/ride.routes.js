3dfd57fcfc3b26795ad3b4dde1f4a5559801921d
Abhi24052004 <at0633058@gmail.com>
2025-11-10 11:25:12 +0530

    Update ride.routes.js

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -1,56 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { body, query } = require('express-validator');
-const rideController = require('../controller/ride.controller');
-const authMiddleware = require('../middlewares/authMidlleware');
+const { body, query } = require("express-validator");
+const rideController = require("../controller/ride.controller");
+const authMiddleware = require("../middlewares/authMidlleware");
 
+router.post("/create", authMiddleware.authUser, rideController.createRide);
 
-router.post('/create',
-    authMiddleware.authUser,
-    rideController.createRide
-)
+router.get("/get-fare", authMiddleware.authUser, rideController.getFare);
 
-router.get('/get-fare',
-    authMiddleware.authUser,
-    rideController.getFare
-)
+router.post(
+  "/confirm",
+  authMiddleware.authCaptain,
+  body("rideId").isMongoId().withMessage("Invalid ride id"),
+  rideController.confirmRide
+);
 
-router.post('/confirm',
-    authMiddleware.authCaptain,
-    body('rideId').isMongoId().withMessage('Invalid ride id'),
-    rideController.confirmRide
-)
+router.get(
+  "/start-ride",
+  authMiddleware.authCaptain,
 
-router.get('/start-ride',
-    authMiddleware.authCaptain,
-    
-    rideController.startRide
-)
+  rideController.startRide
+);
 
-router.post('/end-ride',
-    authMiddleware.authCaptain,
-    body('rideId').isMongoId().withMessage('Invalid ride id'),
-    rideController.endRide
-)
+router.post(
+  "/end-ride",
+  authMiddleware.authCaptain,
+  body("rideId").isMongoId().withMessage("Invalid ride id"),
+  rideController.endRide
+);
 
-router.get('/captainEvent',
-    authMiddleware.authCaptain,
-    rideController.getAllCaptainEvents
-)
+router.get(
+  "/captainEvent",
+  authMiddleware.authCaptain,
+  rideController.getAllCaptainEvents
+);
 
-router.get('/captainRides',
-    authMiddleware.authCaptain,
-    rideController.getAllCaptainRides
-)
+router.get(
+  "/userRides",
+  authMiddleware.authUser,
+  rideController.getAllUserRides
+);
 
-router.get('/userRides',
-    authMiddleware.authUser,
-    rideController.getAllUserRides
-)
+router.post(
+  "/startEventRide",
+  authMiddleware.authCaptain,
+  rideController.startEventRide
+);
 
-router.post('/startEventRide',
-    authMiddleware.authCaptain,
-    rideController.startEventRide
-)
-
+router.post(
+  "/makePaymentForRide",
+  // authMiddleware.authUser,
+  rideController.makePayments
+);
 module.exports = router;
