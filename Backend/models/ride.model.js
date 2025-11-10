15cba0c57b15d5fd8aafa60681b1480bcf618608
Abhi24052004 <at0633058@gmail.com>
2025-11-10 11:26:33 +0530

    Update ride.model.js

--- Backend/models/ride.model.js
+++ Backend/models/ride.model.js
@@ -1,67 +1,75 @@
-const { request } = require('express');
-const mongoose = require('mongoose');
+const { request } = require("express");
+const mongoose = require("mongoose");
 
-
-const rideSchema = new mongoose.Schema({
+const rideSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
     },
     captain: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'captain',
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "captain",
     },
     pickup: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     destination: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     fare: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
 
     status: {
-        type: String,
-        enum: [ 'pending', 'accepted', "ongoing", 'completed', 'cancelled' ],
-        default: 'pending',
+      type: String,
+      enum: ["pending", "accepted", "ongoing", "completed", "cancelled"],
+      default: "pending",
     },
 
     duration: {
-        type: Number,
+      type: Number,
     }, // in seconds
 
     distance: {
-        type: Number,
+      type: Number,
     }, // in meters
 
+    paymentmode: {
+      type: String,
+      enum: ["cash", "online"],
+      required: false,
+    },
+
     paymentID: {
-        type: String,
+      type: String,
     },
     orderId: {
-        type: String,
+      type: String,
     },
     signature: {
-        type: String,
+      type: String,
     },
 
     otp: {
-        type: String,
-        select: false,
-        // required: true,
+      type: String,
+      select: false,
+      // required: true,
     },
     eventDateTime: {
-        type: Date,              
-        required: false,         
+      type: Date,
+      required: false,
     },
-    specialRequest:{
-        type: String,
-        required: false
-    }
-}, { timestamps: true });
+    specialRequest: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('ride', rideSchema);
+module.exports = mongoose.model("ride", rideSchema);
